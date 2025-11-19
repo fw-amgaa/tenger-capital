4881f272ceab8f6e8a14a420b3b14500706619b4
Amgalanbayar Amgalanbaatar <amgalanbayar@Amgalanbayars-MacBook-Air.local>
2025-11-19 19:21:27 +0800

    Addded key figures and guiding principles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,11 +4,17 @@ import LetterFromCeo from "./letter-from-ceo";
 import Footer from "@/components/sections/footer";
 import { TimelineSection } from "./timeline";
 import TeamMembers from "@/components/sections/team-members";
+import KeyFigures from "./key-figures";
+import GuidingPrinciples from "./guiding-principles";
 
 export default function Page() {
     return <ScrollControlProvider>
         <HeroSection />
         <LetterFromCeo />
+
+        <KeyFigures />
+
+        <GuidingPrinciples />
         <TeamMembers showTitle={false} />
 
         <div className="mb-24">
